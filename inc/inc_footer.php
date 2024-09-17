6a9a7e59ba538e9629084e88369fef728bf890ec
Nick <nick@newstarsec.com.au>
2024-09-18 01:34:31 +1000

    09-18-Wednesday

---
+++ inc/inc_footer.php
@@ -0,0 +1,128 @@
+<!-- Start footer -->
+<footer class="custom-footer bg-dark py-5 position-relative" page="inc_footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 mt-4">
+                <div>
+                    <div>
+                        <img src="assets/images/logo-light.png" alt="logo light" height="17" />
+                    </div>
+                    <div class="mt-4 fs-13">
+                        <p>Premium Multipurpose Admin & Dashboard Template</p>
+                        <p>You can build any type of web application like eCommerce, CRM, CMS, Project management apps, Admin Panels, etc using Velzon.</p>
+                        <ul class="list-inline mb-0 footer-social-link">
+                            <li class="list-inline-item">
+                                <a href="javascript: void(0);" class="avatar-xs d-block">
+                                    <div class="avatar-title rounded-circle">
+                                        <i class="ri-facebook-fill"></i>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="javascript: void(0);" class="avatar-xs d-block">
+                                    <div class="avatar-title rounded-circle">
+                                        <i class="ri-github-fill"></i>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="javascript: void(0);" class="avatar-xs d-block">
+                                    <div class="avatar-title rounded-circle">
+                                        <i class="ri-linkedin-fill"></i>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="javascript: void(0);" class="avatar-xs d-block">
+                                    <div class="avatar-title rounded-circle">
+                                        <i class="ri-google-fill"></i>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="javascript: void(0);" class="avatar-xs d-block">
+                                    <div class="avatar-title rounded-circle">
+                                        <i class="ri-dribbble-line"></i>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-7 ms-lg-auto">
+                <div class="row">
+                    <div class="col-sm-4 mt-4">
+                        <h5 class="text-white mb-0">Company</h5>
+                        <div class="text-muted mt-3">
+                            <ul class="list-unstyled ff-secondary footer-list">
+                                <li><a href="pages-profile.php">About Us</a></li>
+                                <li><a href="pages-gallery.php">Gallery</a></li>
+                                <li><a href="pages-team.php">Team</a></li>
+                                <li><a href="pages-pricing.php">Pricing</a></li>
+                                <li><a href="pages-timeline.php">Timeline</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 mt-4">
+                        <h5 class="text-white mb-0">For Jobs</h5>
+                        <div class="text-muted mt-3">
+                            <ul class="list-unstyled ff-secondary footer-list">
+                                <li><a href="apps-job-lists.php">Job List</a></li>
+                                <li><a href="apps-job-application.php">application</a></li>
+                                <li><a href="apps-job-new.php">New Job</a></li>
+                                <li><a href="apps-job-companies-lists.php">Company List</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 mt-4">
+                        <h5 class="text-white mb-0">Support</h5>
+                        <div class="text-muted mt-3">
+                            <ul class="list-unstyled ff-secondary footer-list">
+                                <li><a href="pages-faqs.php">FAQ</a></li>
+                                <li><a href="pages-faqs.php">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row text-center text-sm-start align-items-center mt-5">
+            <div class="col-sm-6">
+                <div>
+                    <p class="copy-rights mb-0">
+                        <script>
+                            document.write(new Date().getFullYear())
+                        </script> © Velzon - Themesbrand
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end mt-3 mt-sm-0">
+                    <ul class="list-inline mb-0 footer-list gap-4 fs-13">
+                        <li class="list-inline-item">
+                            <a href="pages-privacy-policy.php">Privacy Policy</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="pages-term-conditions.php">Terms & Conditions</a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="pages-privacy-policy.php">Security</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- end footer -->
+
+
+<!--start back-to-top-->
+<button onclick="topFunction()" class="btn btn-info btn-icon landing-back-top" id="back-to-top">
+    <i class="ri-arrow-up-line"></i>
+</button>
+<!--end back-to-top-->
---
+++ starter.php
@@ -0,0 +1,30 @@
+<?php include 'layouts/session.php'; ?>
+<?php include 'layouts/main.php'; ?>
+
+<head>
+    <?php includeFileWithVariables('layouts/title-meta.php', array('title' => 'Job Landing')); ?>
+    <!--Swiper slider css-->
+    <link href="assets/libs/swiper/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
+    <?php include 'layouts/head-css.php'; ?>
+</head>
+
+<body data-bs-spy="scroll" data-bs-target="#navbar-example">
+
+    <!-- Begin page -->
+    <div class="layout-wrapper landing">
+        <?php include("inc/inc_navbar.php"); ?>
+        <?php include("inc/inc_footer.php"); ?>
+    </div>
+    <!-- end layout wrapper -->
+
+
+    <?php include 'layouts/vendor-scripts.php'; ?>
+
+    <!--Swiper slider js-->
+    <script src="assets/libs/swiper/swiper-bundle.min.js"></script>
+
+    <!--job landing init -->
+    <script src="assets/js/pages/job-lading.init.js"></script>
+</body>
+
+</html>
